0ac295c23606cb0a44963ce77a25605a4f935fe0
Daniel López Bogaz <lopezbogazdaniel@gmail.com>
2023-11-08 18:20:35 +0100

    sending and receiving chat. not group yet. cant see my messages, only sent from other users

--- client/ett-client/src/App.js
+++ client/ett-client/src/App.js
@@ -1,27 +1,32 @@
 import './App.css';
 import io from 'socket.io-client';
-import { useEffect } from 'react'
+import { useEffect, useState } from 'react'
 
 
 const socket = io.connect('http://localhost:3001')
 
 function App() {
+  const [message, setMessage] = useState("")
+  const [messageReceived, setMessageReceived] =  useState("");
+
 
   const sendMessage = () => {
-    socket.emit("send_message", {message: 'Hello world'}) //the second parameter should come from useState, at the moment is just mock data
+    socket.emit("send_message", {message});
   };
+
 
   
   useEffect(() => {
     socket.on("receive_message", (data) => {
-      alert(data.message);
+      setMessageReceived(data.message);
     })
   }, [socket])
 
   return (
     <div>
-      <input placeholder="Message..."/>
+      <input placeholder="Message..." onChange={(event) => setMessage(event.target.value)}/>
       <button onClick={sendMessage}> Send Message</button>
+      <h1>{messageReceived}</h1>
     </div>
   );
 }
